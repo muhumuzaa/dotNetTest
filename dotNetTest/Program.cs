eb2181c090f5287e93c4364c8f869d989d74b5c2
AMK <AMK@DESKTOP-QL5CJKJ>
2024-04-23 20:46:25 -0400

    Add project files.

---
+++ dotNetTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Pet<T> where T : struct
+{
+	private List<T> petList;
+
+	public Pet()
+	{
+		petList = new List<T>();
+	}
+
+	public void AddPet(T item)
+	{
+		petList.Add(item);
+	}
+
+	public T GetPet(int index)
+	{
+		if (index < 0 || index >= petList.Count)
+		{
+			throw new IndexOutOfRangeException("Index is out of range.");
+		}
+
+		return petList[index];
+	}
+
+	public List<T> GetSortedDescending()
+	{
+		List<T> sortedList = new List<T>(petList);
+		sortedList.Sort();
+		sortedList.Reverse();
+		return sortedList;
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var petCollection = new Pet<int>();
+		petCollection.AddPet(1);
+		petCollection.AddPet(2);
+		petCollection.AddPet(3);
+		petCollection.AddPet(4);
+
+		Console.WriteLine("Items in the original collection:");
+		foreach (int item in petCollection.GetSortedDescending())
+		{
+			Console.WriteLine(item);
+		}
+
+		Console.WriteLine("\nPet at index 2: " + petCollection.GetPet(2));
+	}
+}
